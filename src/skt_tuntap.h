ece05bcdd5fda55ebe2cf0e0399fdd82eacf8ff5
Jim <xboss.sun@foxmail.com>
2022-12-25 18:54:20 +0800

    add tuntap setup

---
+++ src/skt_tuntap.h
@@ -0,0 +1,8 @@
+#ifndef _SKT_TUNTAP_H
+#define _SKT_TUNTAP_H
+
+// #include "skt_utils.h"
+
+int skt_tuntap_open();
+
+#endif  // SKT_TUNTAP_H
--- src/skt_tuntap_osx.c
+++ src/skt_tuntap_osx.c
@@ -1,195 +1,128 @@
 
-#include "skt_tuntap_osx.h"
+#include "skt_tuntap.h"
 
 #ifdef __APPLE__
 
-// #include <arpa/inet.h>
-// #include <errno.h>
-// #include <fcntl.h>
-// #include <stdlib.h>
-// #include <string.h>
-// #include <unistd.h>
-
-// #define SKT_OSX_TAPDEVICE_SIZE 32
-
-// // void tun_close(skt_tuntap_dev_t *device);
-// void skt_tuntap_close(skt_tuntap_dev_t *tuntap) { close(tuntap->fd); }
-
-// int skt_tuntap_open(skt_tuntap_dev_t *device /* ignored */, char *dev, const char *address_mode, /* static or dhcp */
-//                     char *device_ip, char *device_mask, const char *device_mac, int mtu) {
-//     int i;
-//     char tap_device[SKT_OSX_TAPDEVICE_SIZE];
-
-//     for (i = 0; i < 255; i++) {
-//         snprintf(tap_device, sizeof(tap_device), "/dev/tap%d", i);
-
-//         device->fd = open(tap_device, O_RDWR);
-//         if (device->fd > 0) {
-//             LOG_I("Succesfully open %s", tap_device);
-//             break;
-//         }
-//     }
-
-//     if (device->fd < 0) {
-//         LOG_E("Unable to open any tap devices errno: %d %s", errno, strerror(errno));
-//         return -1;
-//     } else {
-//         char buf[256];
-//         FILE *fd;
-
-//         device->ip_addr = inet_addr(device_ip);
-
-//         if (device_mac && device_mac[0] != '\0') {
-//             // FIXME - this is not tested. might be wrong syntax for OS X
-//             // set the hw address before bringing the if up
-//             snprintf(buf, sizeof(buf), "ifconfig tap%d ether %s", i, device_mac);
-//             system(buf);
-//         }
-
-//         snprintf(buf, sizeof(buf), "ifconfig tap%d %s netmask %s mtu %d up", i, device_ip, device_mask, mtu);
-//         system(buf);
-
-//         LOG_I("Interface tap%d up and running (%s/%s)", i, device_ip, device_mask);
-
-//         // read MAC address
-//         snprintf(buf, sizeof(buf), "ifconfig tap%d |grep ether|cut -c 8-24", i);
-//         // traceEvent(TRACE_INFO, "%s", buf);
-
-//         fd = popen(buf, "r");
-//         if (fd < 0) {
-//             skt_tuntap_close(device);
-//             return -1;
-//         } else {
-//             int a, b, c, d, e, f;
-
-//             buf[0] = 0;
-//             fgets(buf, sizeof(buf), fd);
-//             pclose(fd);
-
-//             if (buf[0] == '\0') {
-//                 LOG_W("Unable to read tap%d interface MAC address", i);
-//                 exit(0);
-//             }
-
-//             LOG_I("Interface tap%d [MTU %d] mac %s", i, mtu, buf);
-//             if (sscanf(buf, "%02x:%02x:%02x:%02x:%02x:%02x", &a, &b, &c, &d, &e, &f) == 6) {
-//                 device->mac_addr[0] = a, device->mac_addr[1] = b;
-//                 device->mac_addr[2] = c, device->mac_addr[3] = d;
-//                 device->mac_addr[4] = e, device->mac_addr[5] = f;
-//             }
-//         }
-//     }
-
-//     // read_mac(dev, device->mac_addr);
-
-//     return (device->fd);
-// }
-
-// int skt_tuntap_read(skt_tuntap_dev_t *tuntap, unsigned char *buf, int len) { return (read(tuntap->fd, buf, len)); }
-
-// int skt_tuntap_write(skt_tuntap_dev_t *tuntap, unsigned char *buf, int len) { return (write(tuntap->fd, buf, len)); }
-
-// // // fill out the ip_addr value from the interface, called to pick up dynamic address changes
-// // void skt_tuntap_get_address(skt_tuntap_dev_t *tuntap) {
-// //     // no action
-// // }
-
-// int main(int argc, char *argv[]) {
-//     skt_tuntap_dev_t device;
-//     int fd = skt_tuntap_open(&device, NULL, NULL, "192.168.2.1", "255.255.255.0", NULL, 1400);
-
-//     return 0;
-// }
-
-#include <ctype.h>
 #include <errno.h>
-#include <fcntl.h>
+#include <net/if_utun.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <strings.h>
+#include <stdlib.h>  // exit, etc.
+#include <string.h>
 #include <sys/ioctl.h>
 #include <sys/kern_control.h>
-#include <sys/kern_event.h>
 #include <sys/socket.h>
+#include <sys/sys_domain.h>
+#include <sys/types.h>
+#include <syslog.h>
 #include <unistd.h>
 
-#define UTUN_CONTROL_NAME "com.apple.net.utun_control"
-#define UTUN_OPT_IFNAME 2
+// #define DEV_NAME_PREFIX "utun"
+const char dev_name_prefix[] = "utun";
 
-int open_tun_socket() {
-    struct sockaddr_ctl addr;
-    struct ctl_info info;
-    char ifname[20];
-    socklen_t ifname_len = sizeof(ifname);
-    int fd = -1;
-    int err = 0;
+int skt_tuntap_open(int *dev_name_id) {
+    struct sockaddr_ctl sc;
+    struct ctl_info ctlInfo;
+    int fd;
 
+    memset(&ctlInfo, 0, sizeof(ctlInfo));
+    if (strlcpy(ctlInfo.ctl_name, UTUN_CONTROL_NAME, sizeof(ctlInfo.ctl_name)) >= sizeof(ctlInfo.ctl_name)) {
+        fprintf(stderr, "UTUN_CONTROL_NAME too long");
+        return -1;
+    }
     fd = socket(PF_SYSTEM, SOCK_DGRAM, SYSPROTO_CONTROL);
-    if (fd < 0) return fd;
 
-    bzero(&info, sizeof(info));
-    strncpy(info.ctl_name, UTUN_CONTROL_NAME, MAX_KCTL_NAME);
+    // printf("ctl_name: %s\n", ctlInfo.ctl_name);
 
-    err = ioctl(fd, CTLIOCGINFO, &info);
-    if (err != 0) goto on_error;
-
-    addr.sc_len = sizeof(addr);
-    addr.sc_family = AF_SYSTEM;
-    addr.ss_sysaddr = AF_SYS_CONTROL;
-    addr.sc_id = info.ctl_id;
-    addr.sc_unit = 0;
-
-    err = connect(fd, (struct sockaddr *)&addr, sizeof(addr));
-    if (err != 0) {
-        printf("connect error %d %s\n", errno, strerror(errno));
-        goto on_error;
+    if (fd == -1) {
+        perror("socket(SYSPROTO_CONTROL)");
+        return -1;
     }
-
-    // TODO: forward ifname (we just expect it to be utun0 for now...)
-    err = getsockopt(fd, SYSPROTO_CONTROL, UTUN_OPT_IFNAME, ifname, &ifname_len);
-    if (err != 0) goto on_error;
-
-    printf("dev: %s\n", ifname);
-
-    // There is to close the socket,But in this case I don't need it.
-    // err = fcntl(fd, F_SETFL, O_NONBLOCK);
-    // if (err != 0) goto on_error;
-
-    // fcntl(fd, F_SETFD, FD_CLOEXEC);
-    // if (err != 0) goto on_error;
-
-on_error:
-    if (err != 0) {
+    if (ioctl(fd, CTLIOCGINFO, &ctlInfo) == -1) {
+        perror("ioctl(CTLIOCGINFO)");
         close(fd);
-        return err;
+        return -1;
     }
 
+    sc.sc_id = ctlInfo.ctl_id;
+    sc.sc_len = sizeof(sc);
+    sc.sc_family = AF_SYSTEM;
+    sc.ss_sysaddr = AF_SYS_CONTROL;
+    // sc.sc_unit = 10; /* Only have one, in this example... */
+
+    int i = -1;
+    for (i = 1; i < 255; i++) {
+        sc.sc_unit = i;
+        int c_rt = connect(fd, (struct sockaddr *)&sc, sizeof(sc));
+        if (c_rt == 0) {
+            break;
+        }
+        // else {
+        //     perror("connect(AF_SYS_CONTROL)");
+        // }
+    }
+
+    if (i < 1 || i >= 255) {
+        perror("connect(AF_SYS_CONTROL)");
+        close(fd);
+        return -1;
+    }
+
+    i--;
+    *dev_name_id = i;
+
+    char dev_name[32] = {0};
+    snprintf(dev_name, sizeof(dev_name), "%s%d", dev_name_prefix, i);
+    printf("dev_name: %s\n", dev_name);
+
+    // If the connect is successful, a tun%d device will be created, where "%d"
+    // is our unit number -1
+    // if (connect(fd, (struct sockaddr *)&sc, sizeof(sc)) == -1) {
+    //     perror("connect(AF_SYS_CONTROL)");
+    //     close(fd);
+    //     return -1;
+    // }
     return fd;
 }
 
-int main(int argc, char *argv[]) {
-    int tun_fd = open_tun_socket();
-    printf("tun_fd: %d\n", tun_fd);
+void skt_tuntap_setup(int dev_name_id, char *device_ip) {
+    char buf[256];
+    snprintf(buf, sizeof(buf), "ifconfig %s%d %s %s", dev_name_prefix, dev_name_id, device_ip, device_ip);
+    printf("run: %s", buf);
+    system(buf);
+}
 
-    if (tun_fd < 0) {
-        perror("Allocating interface");
+int main(int argc, char **argv) {
+    int dev_name_id = -1;
+    int utunfd = skt_tuntap_open(&dev_name_id);
+
+    if (utunfd == -1) {
+        fprintf(stderr, "Unable to establish UTUN descriptor - aborting\n");
         exit(1);
     }
 
-    int nread = 0;
-    char buffer[1500];
-    while (1) {
-        nread = read(tun_fd, buffer, sizeof(buffer));
-        if (nread < 0) {
-            perror("Reading from interface");
-            close(tun_fd);
-            exit(1);
-        }
+    skt_tuntap_setup(dev_name_id, "192.168.2.1");
 
-        printf("Read %d bytes from tun/tap device\n", nread);
+    fprintf(stderr, "Utun interface is up.. Configure IPv4 using \"ifconfig utun1 _ipA_ _ipB_\"\n");
+    fprintf(stderr, "                       Configure IPv6 using \"ifconfig utun1 inet6 _ip6_\"\n");
+    fprintf(stderr, "Then (e.g.) ping _ipB_ (IPv6 will automatically generate ND messages)\n");
+
+    // PoC - Just dump the packets...
+    for (;;) {
+        unsigned char c[1500];
+        int len;
+        int i;
+
+        len = read(utunfd, c, 1500);
+
+        // First 4 bytes of read data are the AF: 2 for AF_INET, 1E for AF_INET6, etc..
+        for (i = 4; i < len; i++) {
+            printf("%02x ", c[i]);
+            if ((i - 4) % 16 == 15) printf("\n");
+        }
+        printf("\n");
     }
-    // while (result > 0) {
-    // }
+
+    return (0);
 }
 
 #endif /* __APPLE__ */
