ce874cc7eb835a0c201836218c1412c49cf2c537
Jim <xboss.sun@foxmail.com>
2023-03-24 19:26:03 +0800

    fix fd_map

--- src/skt_proxy_server.c
+++ src/skt_proxy_server.c
@@ -4,25 +4,18 @@
 #include "skt_utils.h"
 #include "uthash.h"
 
-typedef struct skt_fd_entry_s {
+typedef struct {
     int sfd;       // server fd
     int cfd;       // client fd
     uint32_t cid;  // connection id
-    size_t ref_cnt;
-    struct skt_fd_entry_s *next;
+    UT_hash_handle hh;
 } skt_fd_entry_t;
-
-typedef struct {
-    skt_fd_entry_t **buckets;
-    size_t bucket_cnt;
-    size_t size;
-} skt_fd_map_t;
 
 typedef struct {
     struct ev_loop *loop;
     skcp_t *skcp;
     etcp_cli_t *etcp;
-    skt_fd_map_t *sfd_map;
+    skt_fd_entry_t *smap;
     char *target_addr;
     uint16_t target_port;
 } skt_serv_t;
@@ -34,147 +27,59 @@ static skt_serv_t *g_ctx = NULL;
 /* -------------------------------------------------------------------------- */
 
 /* ------------------------------ fd map start ------------------------------ */
+inline static skt_fd_entry_t *find_fd_entry(skt_fd_entry_t *smap, skt_fd_entry_t *cmap, int sfd, int cfd) {
+    skt_fd_entry_t *map = NULL;
+    int fd = 0;
+    if (smap) map = smap;
+    if (cmap) map = cmap;
+    if (sfd) fd = sfd;
+    if (cfd) fd = cfd;
 
-static skt_fd_map_t *fd_map_init(size_t bucket_cnt) {
-    if (bucket_cnt <= 0) {
-        bucket_cnt = 1000;
-    }
-
-    skt_fd_map_t *fd_map = (skt_fd_map_t *)calloc(1, sizeof(skt_fd_map_t));
-    fd_map->bucket_cnt = bucket_cnt;
-    fd_map->buckets = (skt_fd_entry_t **)calloc(bucket_cnt, sizeof(skt_fd_entry_t *));
-    fd_map->size = 0;
-    return fd_map;
-}
-static skt_fd_entry_t *fd_map_find(skt_fd_map_t *fd_map, int sfd, int cfd) {
-    if ((sfd != 0 && cfd != 0) || (sfd == 0 && cfd == 0) || sfd < 0 || cfd < 0) {
-        return NULL;
-    }
-    int fd = sfd;
-    if (cfd > 0) {
-        fd = cfd;
-    }
-
-    size_t key = fd % fd_map->bucket_cnt;
-    skt_fd_entry_t *tmp = fd_map->buckets[key];
-    for (size_t i = 0; tmp && i < fd_map->size; i++) {
-        if (cfd > 0) {
-            if (fd == tmp->cfd) {
-                return tmp;
-            }
-        } else {
-            if (fd == tmp->sfd) {
-                return tmp;
-            }
-        }
-        tmp = tmp->next;
-    }
-    return NULL;
+    skt_fd_entry_t *en = NULL;
+    HASH_FIND_INT(map, &fd, en);
+    return en;
 }
 
-static int fd_map_add(skt_fd_map_t *fd_map, int fd, skt_fd_entry_t *entry) {
-    if (fd < 0) {
-        return -1;
-    }
+inline static void add_fd_entry(skt_fd_entry_t **smap, skt_fd_entry_t **cmap, int sfd, int cfd, uint32_t cid) {
+    skt_fd_entry_t *en = NULL;
 
-    size_t key = fd % fd_map->bucket_cnt;
-    skt_fd_entry_t *tmp = fd_map->buckets[key];
+    skt_fd_entry_t *tmp = find_fd_entry(*smap, NULL, sfd, 0);
     if (!tmp) {
-        fd_map->buckets[key] = entry;
-        goto fd_map_add_end;
+        en = (skt_fd_entry_t *)calloc(1, sizeof(skt_fd_entry_t));
+        en->sfd = sfd;
+        en->cfd = cfd;
+        en->cid = cid;
+        HASH_ADD_INT(*smap, sfd, en);
     }
 
-    for (size_t i = 0; tmp; i++) {
-        if (!tmp->next) {
-            break;
-        }
-        tmp = tmp->next;
-        if (i >= fd_map->size) {
-            return -1;
-        }
-    }
-    tmp->next = entry;
-
-fd_map_add_end:
-    entry->next = NULL;
-    entry->ref_cnt++;
-    fd_map->size++;
-    return 0;
-}
-
-static void fd_map_free_entry(skt_fd_entry_t *entry) {
-    if (!entry) {
-        return;
-    }
-    if (entry->ref_cnt > 0) {
-        entry->ref_cnt--;
-    }
-    if (entry->ref_cnt == 0) {
-        free(entry);
+    tmp = find_fd_entry(*cmap, NULL, 0, cfd);
+    if (!tmp) {
+        en = (skt_fd_entry_t *)calloc(1, sizeof(skt_fd_entry_t));
+        en->sfd = sfd;
+        en->cfd = cfd;
+        en->cid = cid;
+        HASH_ADD_INT(*cmap, cfd, en);
     }
 }
 
-static int fd_map_del(skt_fd_map_t *fd_map, int sfd, int cfd) {
-    if ((sfd != 0 && cfd != 0) || (sfd == 0 && cfd == 0) || sfd < 0 || cfd < 0) {
-        return -1;
-    }
-    int fd = sfd;
-    if (cfd > 0) {
-        fd = cfd;
-    }
-
-    size_t key = fd % fd_map->bucket_cnt;
-    skt_fd_entry_t *tmp = fd_map->buckets[key];
-    skt_fd_entry_t *last = NULL;
-    for (size_t i = 0; tmp && i < fd_map->size; i++) {
-        if (cfd > 0) {
-            if (fd == tmp->cfd) {
-                if (last == NULL) {
-                    fd_map->buckets[key] = NULL;
-                } else {
-                    last->next = tmp->next;
-                }
-                fd_map->size--;
-                fd_map_free_entry(tmp);
-                return 0;
-            }
-        } else {
-            if (fd == tmp->sfd) {
-                if (last == NULL) {
-                    fd_map->buckets[key] = NULL;
-                } else {
-                    last->next = tmp->next;
-                }
-                fd_map->size--;
-                fd_map_free_entry(tmp);
-                return 0;
-            }
-        }
-        last = tmp;
-        tmp = tmp->next;
-    }
-    return -1;
-}
-
-static void fd_map_free(skt_fd_map_t *fd_map) {
-    if (!fd_map) {
-        return;
-    }
-    if (fd_map->bucket_cnt <= 0 || fd_map->size <= 0 || !fd_map->buckets) {
-        return;
-    }
-
-    for (size_t i = 0; i < fd_map->bucket_cnt; i++) {
-        skt_fd_entry_t *en = fd_map->buckets[i];
-        for (size_t j = 0; en && j < fd_map->size; j++) {
-            skt_fd_entry_t *tmp = en;
-            en = en->next;
-            fd_map_free_entry(tmp);
+inline static void del_fd_entry(skt_fd_entry_t **smap, skt_fd_entry_t **cmap, int sfd, int cfd) {
+    skt_fd_entry_t *en = NULL;
+    if (smap && *smap) {
+        HASH_FIND_INT(*smap, &sfd, en);
+        if (en) {
+            HASH_DEL(*smap, en);
+            free(en);
         }
     }
 
-    free(fd_map->buckets);
-    free(fd_map);
+    en = NULL;
+    if (cmap && *cmap) {
+        HASH_FIND_INT(*cmap, &cfd, en);
+        if (en) {
+            HASH_DEL(*cmap, en);
+            free(en);
+        }
+    }
 }
 /* ------------------------------- fd map end ------------------------------- */
 
@@ -199,6 +104,8 @@ static void on_recv_seg_data(uint32_t cid, skt_seg_t *seg) {
     }
 
     skcp_conn_t *conn = skcp_get_conn(g_ctx->skcp, cid);
+    skt_fd_entry_t *cmap = (skt_fd_entry_t *)conn->user_data;
+
     if (cmd == SKT_MSG_CMD_ACCEPT) {
         int sfd = etcp_client_create_conn(g_ctx->etcp, g_ctx->target_addr, g_ctx->target_port, NULL);
         if (sfd <= 0) {
@@ -212,29 +119,17 @@ static void on_recv_seg_data(uint32_t cid, skt_seg_t *seg) {
         //     return;
         // }
 
-        skt_fd_entry_t *en = (skt_fd_entry_t *)calloc(1, sizeof(skt_fd_entry_t));
-        en->ref_cnt = 0;
-        en->sfd = sfd;
-        en->cfd = cfd;
-        en->cid = cid;
-        fd_map_add(g_ctx->sfd_map, sfd, en);
-        skt_fd_map_t *cfd_map = (skt_fd_map_t *)conn->user_data;
-        if (!cfd_map) {
-            cfd_map = fd_map_init(0);
-            conn->user_data = cfd_map;
-        }
-        fd_map_add(cfd_map, cfd, en);
-        // LOG_I("skcp cmd accept sfd: %d cfd: %d", en->sfd, en->cfd);
+        add_fd_entry(&g_ctx->smap, &cmap, sfd, cfd, cid);
+        conn->user_data = cmap;
+        // TODO: test
+        int smap_cnt = HASH_COUNT(g_ctx->smap);
+        int cmap_cnt = HASH_COUNT(cmap);
+        LOG_I("skcp cmd accept sfd: %d cfd: %d smap_cnt: %d cmap_cnt: %d", sfd, cfd, smap_cnt, cmap_cnt);
 
         return;
     }
 
-    skt_fd_map_t *cfd_map = (skt_fd_map_t *)conn->user_data;
-    if (!cfd_map) {
-        LOG_E("on_recv_seg_data cfd_map error cfd: %d", cfd);
-        return;
-    }
-    skt_fd_entry_t *entry = fd_map_find(cfd_map, 0, cfd);
+    skt_fd_entry_t *entry = find_fd_entry(NULL, cmap, 0, cfd);
     if (!entry) {
         LOG_E("on_recv_seg_data find cfd_map error cfd: %d cid: %u type: %x cmd: %c", cfd, cid, seg->type, cmd);
         return;
@@ -243,8 +138,8 @@ static void on_recv_seg_data(uint32_t cid, skt_seg_t *seg) {
     if (cmd == SKT_MSG_CMD_CLOSE) {
         etcp_client_close_conn(g_ctx->etcp, entry->sfd, 1);
         // LOG_I("skcp cmd close sfd: %d cfd: %d", entry->sfd, cfd);
-        fd_map_del(cfd_map, 0, cfd);
-        fd_map_del(g_ctx->sfd_map, entry->sfd, 0);
+        del_fd_entry(&g_ctx->smap, &cmap, entry->sfd, entry->cfd);
+        conn->user_data = cmap;
         return;
     }
 
@@ -266,7 +161,7 @@ static void on_recv_seg_data(uint32_t cid, skt_seg_t *seg) {
 /* ---------------------------- EasyTCP callback ---------------------------- */
 
 static void on_tcp_recv(int fd, char *buf, int len) {
-    skt_fd_entry_t *entry = fd_map_find(g_ctx->sfd_map, fd, 0);
+    skt_fd_entry_t *entry = find_fd_entry(g_ctx->smap, NULL, fd, 0);
     if (!entry) {
         LOG_E("on_tcp_recv find sfd_map error sfd: %d", fd);
         return;
@@ -294,7 +189,7 @@ static void on_tcp_recv(int fd, char *buf, int len) {
 
 static void on_tcp_close(int fd) {
     LOG_D("tcp client on_close fd: %d", fd);
-    skt_fd_entry_t *entry = fd_map_find(g_ctx->sfd_map, fd, 0);
+    skt_fd_entry_t *entry = find_fd_entry(g_ctx->smap, NULL, fd, 0);
     if (!entry) {
         LOG_E("on_tcp_close find sfd_map error sfd: %d", fd);
         return;
@@ -313,18 +208,16 @@ static void on_tcp_close(int fd) {
     }
 
     // LOG_I("on_tcp_close sfd: %d cfd: %d", entry->sfd, entry->cfd);
-    fd_map_del(g_ctx->sfd_map, fd, 0);
+
     skcp_conn_t *conn = skcp_get_conn(g_ctx->skcp, entry->cid);
     if (!conn) {
         LOG_E("on_tcp_close skcp_get_conn error sfd: %u", entry->cid);
         return;
     }
-    skt_fd_map_t *cfd_map = (skt_fd_map_t *)conn->user_data;
-    if (!cfd_map) {
-        LOG_E("on_tcp_close cfd_map error cfd: %d", entry->cfd);
-        return;
-    }
-    fd_map_del(cfd_map, 0, entry->cfd);
+
+    skt_fd_entry_t *cmap = (skt_fd_entry_t *)conn->user_data;
+    del_fd_entry(&g_ctx->smap, &cmap, entry->sfd, entry->cfd);
+    conn->user_data = cmap;
 }
 
 /* ------------------------------ skcp callback ----------------------------- */
@@ -384,8 +277,12 @@ static void skcp_on_close(uint32_t cid) {
     LOG_D("skcp_on_close cid: %u", cid);
     skcp_conn_t *conn = skcp_get_conn(g_ctx->skcp, cid);
     if (conn && conn->user_data) {
-        skt_fd_map_t *cfd_map = (skt_fd_map_t *)conn->user_data;
-        fd_map_free(cfd_map);
+        skt_fd_entry_t *cmap = (skt_fd_entry_t *)conn->user_data;
+        skt_fd_entry_t *en, *tmp;
+        HASH_ITER(hh, cmap, en, tmp) {
+            HASH_DEL(cmap, en);
+            free(en);
+        }
         conn->user_data = NULL;
     }
     return;
@@ -406,7 +303,7 @@ int skt_proxy_server_init(skcp_conf_t *skcp_conf, etcp_cli_conf_t *etcp_conf, st
     g_ctx->loop = loop;
     g_ctx->target_addr = target_addr;
     g_ctx->target_port = target_port;
-    g_ctx->sfd_map = fd_map_init(0);
+    g_ctx->smap = NULL;
 
     // g_ctx->ip_cid_ht = NULL;
 
@@ -441,9 +338,13 @@ void skt_proxy_server_free() {
         etcp_free_client(g_ctx->etcp);
     }
 
-    if (g_ctx->sfd_map) {
-        fd_map_free(g_ctx->sfd_map);
-        g_ctx->sfd_map = NULL;
+    if (g_ctx->smap) {
+        skt_fd_entry_t *en, *tmp;
+        HASH_ITER(hh, g_ctx->smap, en, tmp) {
+            HASH_DEL(g_ctx->smap, en);
+            free(en);
+        }
+        g_ctx->smap = NULL;
     }
 
     FREE_IF(g_ctx);
