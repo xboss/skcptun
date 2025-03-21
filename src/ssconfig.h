6fb69bc348dc198ee324fcfdfac2f4f5cdb0efd6
Jim <xboss.sun@foxmail.com>
2025-03-21 13:15:20 +0800

    Update ssconfig.h

--- src/ssconfig.h
+++ src/ssconfig.h
@@ -3,6 +3,7 @@
 
 #include <stddef.h>
 
+typedef int (*sscf_cb_t)(const char* key, const char* value, size_t line_no, void* user);
 
 void sscf_setup(size_t max_line_size);
 int sscf_parse(const char* filename, sscf_cb_t handler, void* user);
